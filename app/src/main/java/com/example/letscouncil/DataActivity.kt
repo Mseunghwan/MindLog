2b593eafef187a9e3b5dbdaebdf74223dd5b3298
Mseunghwan <minccc42710@gmail.com>
2025-01-17 14:50:03 +0900

    go

--- app/src/main/java/com/example/letscouncil/DataActivity.kt
+++ app/src/main/java/com/example/letscouncil/DataActivity.kt
@@ -1,7 +1,6 @@
 package com.example.letscouncil
 
 import android.os.Bundle
-import android.transition.TransitionInflater
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -20,10 +19,6 @@ class DataActivity : AppCompatActivity() {
     private lateinit var diaryAdapter: DiaryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // 액티비티 전환 애니메이션 설정
-        window.exitTransition = TransitionInflater.from(this)
-            .inflateTransition(R.transition.activity_fade)
-
         super.onCreate(savedInstanceState)
         binding = ActivityDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
